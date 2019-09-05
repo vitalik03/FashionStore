c004264363a85110993262ce9a7bb5e02fdb3b9d
Vitalik <vitalik.vovryk@gmail.com>
2019-09-05 16:13:54 +0300

    cover by unit tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './users.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -14,5 +15,13 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw user not found after update user with id 1', async () => {
+    await service.update('1', new User())
+      .catch((error) => {
+        expect(error.status).toBe(404);
+        expect(error.message).toBe("Not found");
+    });
   });
 });
