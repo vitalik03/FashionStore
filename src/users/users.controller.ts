c95ee1f99e283af19d4174730b01470ebace0381
Vitalik <vitalik.vovryk@gmail.com>
2019-09-05 14:52:10 +0300

    Add method Post and  endpoint /users,

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { IUser } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+
+
+
+
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService){}
+
+    @Post()
+	async create(@Body() createUser: CreateUserDto): Promise<IUser>{
+		return await this.usersService.create(createUser);
+    }
+  
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { IUser } from './interfaces/user.interface';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+
+
+
+export class UsersService {
+    constructor(
+		@Inject('USER_REPOSITORY')
+		private readonly userRepository: Repository<IUser>,
+	){}
+
+    async create(user: IUser): Promise<IUser>{
+		return await this.userRepository.save(user);
+	}
+}
