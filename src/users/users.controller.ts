caf2f3fa8e1f015e5812c88dca0200a82ac770a3
Roman <romanlvivskij@gmail.com>
2019-09-05 16:12:12 +0300

    Created method Get with ids and endpoint /users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { IUser } from './interfaces/user.interface';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService){}
+    
+    @Get(':id')
+    async getById(@Param('id') id: string): Promise<IUser>{
+        return await this.usersService.getById(id);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { IUser } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @Inject('USER REPOSITORY')
+        private readonly userRepository: Repository<IUser>,
+    ){}
+
+    async getById(id:string): Promise<IUser>{
+        return await this.userRepository.findOne(id);
+    }
+}
