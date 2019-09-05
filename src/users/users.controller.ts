8d539572fd5f18dd14421ef71683ec54327e79d0
Vitalik <vitalik.vovryk@gmail.com>
2019-09-05 16:01:26 +0300

    add endpoint /users/:id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Put, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService){}
+    
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() updateUser: CreateUserDto){
+		return await this.usersService.update(id, updateUser);
+	}
+
+}
