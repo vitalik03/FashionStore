c333502ac5247a58fd3475acd65c01119b84c8d8
Vitalik <vitalik.vovryk@gmail.com>
2019-09-05 16:28:29 +0300

    add endpoint /users/^id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService){}
+
+	@Delete(':id')
+	async delete(@Param('id') id:string){
+		return await this.usersService.delete(id);
+	}
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { IUser } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+		@Inject('USER_REPOSITORY')
+		private readonly userRepository: Repository<IUser>,
+	){}
+
+	async delete(id: string){
+		return await this.userRepository.delete(id);
+	}
+
+}
