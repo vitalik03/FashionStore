5a0577429b584bb22e805bbc8b24017b5874b9b3
Vitalik <vitalik.vovryk@gmail.com>
2019-09-07 10:21:41 +0300

    Add verification for method create

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { IUser } from './interfaces/user.interface';
 import { Repository } from 'typeorm';
 
@@ -12,7 +12,17 @@ export class UsersService {
 		private readonly userRepository: Repository<IUser>,
 	){}
 
-    async create(user: IUser): Promise<IUser>{
+    async create( user: IUser ): Promise<IUser> {
+		const firstName = user.firstName;
+		const params = {
+			firstName
+		}
+		const testUser = await this.userRepository.findOne(params);
+		
+		if( testUser && testUser.lastName === user.lastName ){
+			throw new HttpException("User has already been created", HttpStatus.FOUND);
+		}
+
 		return await this.userRepository.save(user);
 	}
 }
