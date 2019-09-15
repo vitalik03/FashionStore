cfb8cfc44a52f9c42b6dd4cd584979c167d691c5
Roman <romanlvivskij@gmail.com>
2019-09-15 20:18:31 +0300

    Added Get method

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { IOrder } from './interfaces/order.interface';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+    constructor(private readonly ordersService: OrdersService){}
+
+    @Get()
+    async get():Promise<IOrder[]>{
+        return await this.ordersService.getAll();
+    }
+}
