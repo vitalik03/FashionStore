dba919bb5cb126f85e7217baf14e0abd8ac52e5f
Roman <romanlvivskij@gmail.com>
2019-09-17 23:06:40 +0300

    Merged Get orders(fully)

--- src/selected-products/dto/create-sp.dto.ts
+++ src/selected-products/dto/create-sp.dto.ts
@@ -2,10 +2,5 @@ export class CreateSelectedProductDto{
     readonly id: number;
     readonly price: number;
     readonly quantity: number;
-<<<<<<< HEAD
     readonly orderId :number;
 }
-=======
-    readonly orderId: number;
-}
->>>>>>> getOrders
