476b4763bf07addae56086718e6ba6f974cf12c0
Vitalik <vitalik.vovryk@gmail.com>
2019-09-19 14:25:21 +0300

    Added Cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  const whitelist = ['http://localhost:4200'];
+  const corsOptions = {
+    credentials: true, 
+    origin: (origin, callback) => {
+      if(whitelist.includes(origin))
+        return callback(null, true)
+        callback(new Error('Not allowed by CORS'));
+    }
+  }
+  app.use(cors());
+  app.use(cookieParser());
+  // app.use(csurf({ cookie: true }));
+
   app.useGlobalPipes(new ValidationPipe());
   const options = new DocumentBuilder()
   .setTitle('Users API')
--- src/selected-products/dto/create-sp.dto.ts
+++ src/selected-products/dto/create-sp.dto.ts
@@ -12,4 +12,5 @@ export class CreateSelectedProductDto{
 
     @ApiModelProperty()
     readonly orderId :number;
+    
 }
