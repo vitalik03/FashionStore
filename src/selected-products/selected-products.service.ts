24106b605f2b4cf8a82abed1eb4e4eaa94602cdd
Roman <romanlvivskij@gmail.com>
2019-09-23 22:15:10 +0300

    Added function for sum calculation

--- src/selected-products/selected-products.service.ts
+++ src/selected-products/selected-products.service.ts
@@ -13,4 +13,13 @@ export class SelectedProductsService {
     async create(selectedProduct: CreateSelectedProductDto){
         return await this.selectedProductRepository.save(selectedProduct);
     }
+
+    async sumOfProducts(id : string){
+        const products = await this.selectedProductRepository.find();
+        var sum = 0.0;
+        for(let i of products){
+            sum += i.price;
+        }
+        return sum;
+    }
 }
