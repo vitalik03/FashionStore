4345a14ea22a04759f171a4d337faf18c9560910
Roman <romanlvivskij@gmail.com>
2019-09-29 15:27:24 +0300

    Now /me returns all info about user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,15 @@ import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { UserLogin } from '../users/dto/create-user.dto';
 import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger';
+import { UsersService } from 'src/users/users.service';
 
 @ApiUseTags('auth')
 @Controller()
 export class AuthController {
 
     constructor(
-        private readonly authService: AuthService
+        private readonly authService: AuthService,
+        private readonly usersService: UsersService
     ) {}
 
     @Post('login')
@@ -21,7 +23,9 @@ export class AuthController {
     @UseGuards(AuthGuard('jwt'))
     @Get('me')
     getProfile(@Request() req) {
-        return req.user;
+        const user = req.user;
+        const userOne = this.usersService.findOne(user.email);
+        return userOne;
     }
 
 }
